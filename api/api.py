a9d13402910e7b98aa7047aa95c00b95ee086331
Zomlik <sanek77594@yandex.ru>
2023-09-24 08:26:33 +0300

    added get and allure

--- api/api.py
+++ api/api.py
@@ -8,10 +8,19 @@ class Api:
     def __init__(self):
         self.response = None
 
+    # METOD
+    @allure.step("Отправка запроса Get")
+    def get(self, url: str, endpoint: str):
+        with allure.step(f"Get запрос на:{url}{endpoint}"):
+            self.response = requests.get(url=f"{url}{endpoint}",
+                                         timeout=self._TIMEOUT)
+        return self
+
     @allure.step("Отправка запроса Post")
-    def post(self, url, endpoint, params: None, json_body):
+    def post(self, url: str, endpoint: str, params: dict = None, json_body: dict = None):
         self.response = requests.post(url=f"{url}{endpoint}",
                                       params=params,
                                       json=json_body,
                                       timeout=self._TIMEOUT)
         return self
+
