85f58dece7b0ea7e0f910ebfd641e3768c0871c0
Zomlik <sanek77594@yandex.ru>
2023-09-25 15:07:40 +0300

    update create test

--- data/create_pet_data.py
+++ data/create_pet_data.py
@@ -2,5 +2,8 @@ from model.create_user_model import RequestCreateUserModel
 
 
 # данные для тестов ('request_parameters')
-data = (RequestCreateUserModel(id=15, name="Pet"),)
+data = (RequestCreateUserModel(id=65, name="Doggi", category={"id": 35, "name": "Jojo"},
+                               photourls="HHT", tags=None),
 
+        )
+#{"id": 36, "name": "Lol"}
--- model/create_user_model.py
+++ model/create_user_model.py
@@ -6,6 +6,9 @@ class RequestCreateUserModel:
     """Класс для параметров rquest"""
     id: int
     name: str
+    category: dict
+    photourls: str
+    tags: dict
 
     def to_dict(self):
         """преобразование в dict для отправки body"""
